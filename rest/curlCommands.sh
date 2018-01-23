f386642c0c5179e7abe24f54ae9cbcf8b0d4af0c
Carlos Patino <carlospatinos@gmail.com>
2018-01-23 20:01:10 +0000

    Se agregan algunos comandos de curl para hacer pruebas

---
+++ rest/curlCommands.sh
@@ -0,0 +1,14 @@
+SERVER="172.17.0.1:8080"
+
+curl -X PUT -H "Content-Type: application/json" -d '{"name":"chris","age":30}' http://$SERVER/dist-http-example/1
+curl -X PUT -H "Content-Type: application/json" -d '{"name":"adam","age":26}' http://$SERVER/dist-http-example/2
+
+
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://$SERVER/dist-http-example/1.json
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://$SERVER/dist-http-example/1.xml
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://$SERVER/dist-http-example/?q=name is 'adam'
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://$SERVER/dist-http-example/;p=name
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET "http://$SERVER/dist-http-example/count()"
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET "http://$SERVER/dist-http-example/double-average(age)"
+
+#https://docs.oracle.com/cd/E24290_01/coh.371/e22839/rest_example.htm#COHCG5237
