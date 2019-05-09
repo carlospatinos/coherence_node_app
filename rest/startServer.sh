72f84c410fc4e6b645321f66fb99aababefa9799
Carlos Patino <carlos.patino_soria@genesys.com>
2019-05-09 19:16:21 +0100

    Jira Ticket: * Why: *

--- rest/startServer.sh
+++ rest/startServer.sh
@@ -1,4 +1,4 @@
-COHERENCE_HOME=/home/ecapati/development/Oracle/Middleware/Oracle_Home/coherence
+COHERENCE_HOME=/home/carlospatinos/development/Oracle/Middleware/Oracle_Home/coherence
 
 #java -cp .:./lib/*:$COHERENCE_HOME/lib/coherence.jar:$COHERENCE_HOME/lib/coherence-rest.jar  -Dtangosol.coherence.cacheconfig=example-server-config.xml com.tangosol.net.DefaultCacheServer
 
